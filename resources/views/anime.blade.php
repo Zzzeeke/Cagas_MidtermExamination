6a9e72724b5383b23fcbd1dee1eb10a5e0c2c77e
Zzzeeke <zekecagas@gmail.com>
2024-06-20 18:45:17 +0800

    Created route, view, and controller

---
+++ resources/views/anime.blade.php
@@ -0,0 +1,13 @@
+<div>
+   <!DOCTYPE html>
+   <html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Viewport" content="width=device-width, initial-scale=1.0">
+        <title>Anime</title>
+    </head>
+    <body>
+        
+    </body>
+   </html>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anime', function () {
+    return view('anime');
+})->name('anime');
